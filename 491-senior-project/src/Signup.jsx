f2b972a085e08d5bccf6fe8fc6e841dd2e18ec90
vicperez208 <vicperez208@gmail.com>
2024-09-10 00:39:55 -0700

    Added Signup Page
    Added routing to some pages and signup page. New initial homescreen

---
+++ 491-senior-project/src/Signup.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import './Signup.css';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic for handling signup here
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <section className="signup-container">
+      <div className="signup-content">
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="continue-button">Continue</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
--- 491-senior-project/src/main.jsx
+++ 491-senior-project/src/main.jsx
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './Login.jsx';
 import Homepage from './Homepage.jsx';
+import Signup from './Signup.jsx'; // Import the new Signup page
 import './index.css';
 
 const App = () => (
   <Router>
-    <nav>
-      <ul>
-        <li><Link to="/">Login</Link></li>
-        <li><Link to="/homepage">Homepage</Link></li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><Link to="/">Login</Link></li>
+        <li className="nav-item"><Link to="/homepage">Homepage</Link></li>
+        <li className="nav-item"><Link to="/signup">Sign Up</Link></li> {/* Added Sign Up link */}
       </ul>
     </nav>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/homepage" element={<Homepage />} />
+      <Route path="/signup" element={<Signup />} /> {/* Added route for Signup */}
     </Routes>
   </Router>
 );
