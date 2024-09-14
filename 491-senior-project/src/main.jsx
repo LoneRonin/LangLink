e4b962a5eee138bfdf6b0c2b28c70094c5b72cec
ZachHunt <zachhunt437@gmail.com>
2024-09-13 23:26:11 -0700

    Replaced Grammar with Conjugate. and Finished Conjugate Practice Skeleton

--- 491-senior-project/src/main.jsx
+++ 491-senior-project/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './Login.jsx';
 import Homepage from './Homepage.jsx';
 import Signup from './Signup.jsx'; // Import the new Signup page
-import Grammar from './Grammar.jsx';
+import Conjugate from './Conjugate.jsx';
 import './index.css';
 
 const App = () => (
@@ -14,14 +14,14 @@ const App = () => (
         <li className="nav-item"><Link to="/">Login</Link></li>
         <li className="nav-item"><Link to="/homepage">Homepage</Link></li>
         <li className="nav-item"><Link to="/signup">Sign Up</Link></li> {/* Added Sign Up link */}
-        <li className="nav-item"><Link to="/grammar">Grammar</Link></li>
+        <li className="nav-item"><Link to="/Conjugate">Conjugate</Link></li>
       </ul>
     </nav>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/signup" element={<Signup />} /> {/* Added route for Signup */}
-      <Route path="/grammar" element={<Grammar />} />
+      <Route path="/Conjugate" element={<Conjugate />} />
     </Routes>
   </Router>
 );
