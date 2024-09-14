60abbd817aa0a8c4539f2a082c7ca2523f6b7223
Tris1997 <Tris22@verizon.net>
2024-09-14 01:28:24 -0700

    Updated backend code
    Updated backend code for languageselection to be stored into the firebase database. Also merged code with what victor had.

--- 491-senior-project/src/main.jsx
+++ 491-senior-project/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './Login/Login.jsx';
 import Homepage from './Homepage/Homepage.jsx';
-import Signup from './Signup/Signup.jsx'; // Import the new Signup page
+import Signup from './Signup/Signup.jsx';
+import LanguageSelection from './LanguageSelection/LanguageSelection.jsx';
+import Profile from './Profile/Profile.jsx'; // Import the Profile page
 import './index.css';
 
 const App = () => (
@@ -12,13 +14,16 @@ const App = () => (
       <ul className="nav-list">
         <li className="nav-item"><Link to="/">Log In</Link></li>
         <li className="nav-item"><Link to="/homepage">Homepage</Link></li>
-        <li className="nav-item"><Link to="/signup">Sign Up</Link></li> {/* Added Sign Up link */}
+        <li className="nav-item"><Link to="/signup">Sign Up</Link></li>
+        <li className="nav-item"><Link to="/profile">Profile</Link></li>
       </ul>
     </nav>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/homepage" element={<Homepage />} />
-      <Route path="/signup" element={<Signup />} /> {/* Added route for Signup */}
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/language-selection" element={<LanguageSelection />} />
+      <Route path="/profile" element={<Profile />} /> {/* Added route for Profile */}
     </Routes>
   </Router>
 );
