cc5e1e4bf5e406724a23b22c13d111a7a44d126e
diego <72894762+meatballmyers@users.noreply.github.com>
2024-09-18 20:36:57 -0700

    Update Friends.jsx

--- 491-senior-project/src/Friends/Friends.jsx
+++ 491-senior-project/src/Friends/Friends.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 import { Link } from 'react-router-dom';
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from '../firebase';
@@ -6,22 +8,30 @@ import './Friends.css';
 
 
 const Friends = () => {
-    
-    const loadTest = async() => {
+    const [friendModel, setFriend] = useState({
+        email: "",
+        firstName: "",
+        lastName: ""
+    })
+
+    useEffect(() => {
         try{
+            //friends = new Set([]);
             console.log("initializing");
             const usersList = collection(db, "users");
             const q = query(usersList, where("language", "==", "spanish"));
         
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-            return(console.log(doc.id, " => ", doc.data()));
+            getDocs(q).then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.id, " => ", doc.data());
+                });
             });
+            
         }
         catch(err){
-            return(console.log("Error loading user list.", err));
+            console.log("Error loading user list.", err);
         }
-    }
+    }, []);
 
     //document.getElementById("friendDisplay").onload = function() {loadTest()};
 
@@ -29,7 +39,7 @@ const Friends = () => {
         <section className='friendsList' id="friendsList">
             <div id='friendDisplay'>
                 <h2>Fwiends</h2>
-                <p onLoad={loadTest()}>Welcome to the friend zone</p>
+                <p>Welcome to the friend zone</p>
             </div>
         </section>
     );
